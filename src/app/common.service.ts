b8b10174b353a9b6f14074a33003ac15711871f9
PriyankaRoy <priyanka.pink@gmail.com>
2017-05-21 22:00:32 +0530

    Update common.service.ts

--- src/app/common.service.ts
+++ src/app/common.service.ts
@@ -7,29 +7,25 @@ import 'rxjs/add/operator/map';
 export class CommonService {
  response :any;
      constructor(private http: Http) { }
-
-// serviceRequest(apiURL , type , data){
-//     return this.response = this.http[type](baseURL+apiURL,data)
-//       .map(response => response.json());
-// }
-getCheckInServiceData(lang) {
-   let headers = new Headers();
-   headers.append('Content-Type', 'application/json; charset=utf-8');
-  headers.append("Accept-Language", lang);
-   headers.append("Access-Control-Allow-Origin", "*");
   
-    return this.http.get('http://localhost:3000/newapp/checkin',{
-      headers: headers
-    })
-      .map(response => response.json());
-  }
+ // Get service not used 
+// getCheckInServiceData(lang) {
+//    let headers = new Headers();
+//    headers.append('Content-Type', 'application/json; charset=utf-8');
+//   headers.append("Accept-Language", lang);
+//    headers.append("Access-Control-Allow-Origin", "*");
+  
+//     return this.http.get('http://localhost:3000/newapp/checkin',{
+//       headers: headers
+//     })
+//       .map(response => response.json());
+//   }
 
 postCheckInDetails(checkin,lang) {
    let headers = new Headers();
    headers.append('Content-Type', 'application/json; charset=utf-8');
    headers.append('Accept', 'application/json; charset=utf-8');
     headers.append("Accept-Language", lang);
- // headers.append("Accept-Language", checkin);
    headers.append("Access-Control-Allow-Origin", "*");
   
     return this.http.post('http://localhost:3000/newapp/checkin',checkin,{
