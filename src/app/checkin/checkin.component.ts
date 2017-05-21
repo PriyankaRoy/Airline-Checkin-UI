29ac20cf1674016b7732f4a88e671834e0c913d6
PriyankaRoy <priyanka.pink@gmail.com>
2017-05-21 21:56:43 +0530

    Update checkin.component.ts

--- src/app/checkin/checkin.component.ts
+++ src/app/checkin/checkin.component.ts
@@ -4,79 +4,55 @@ import { CheckinDetailsModel } from './models/checkin-details.model';
 import { CommonService } from '../common.service';
 import { NgForm } from "@angular/forms";
 
+import { TranslateService } from "ng2-translate";
 
+const defaultLanguage = "en";
+const additionalLanguages = ["nl"];
+const languages: string[] = [defaultLanguage].concat(additionalLanguages);
 @Component({
   selector: 'app-checkin',
   templateUrl: './checkin.component.html',
   styleUrls: ['./checkin.component.css'],
   providers: [CommonService]
 })
+
 export class CheckinComponent implements OnInit {
 
-
-  pageDetailsModels: PageDetailsModel = new PageDetailsModel();
   checkinDetailsModels: CheckinDetailsModel = new CheckinDetailsModel();
   pageData;
-  lang = 'en';
+  lang:string='en';
   pageError;
   pageSuccess;
-  constructor(private checkinService: CommonService) { this.pageError = ""; this.pageSuccess = "" };
+  langs: string[];
+  constructor(private checkinService: CommonService, private translate: TranslateService) { this.pageError = ""; this.pageSuccess = "" };
 
   onChange() {
     this.pageError = ""; this.pageSuccess = ""
-    this.serviCall();
+    this.translate.use(this.lang);
+
+
   }
 
   ngOnInit() {
-    this.serviCall();
-  }
-
-  serviCall() {
-    this.checkinService.getCheckInServiceData(this.lang)
-      .subscribe(
-      pageData => { console.log('Completed pageDataLoad!'); this.pageData = pageData, this.createModelData() },
-      error => console.error('Error: '),
-      () => console.log('Completed pageDataLoad!', this.pageData)
-      );
+    this.langs = languages;
+    this.translate.setDefaultLang(defaultLanguage);
+    this.translate.addLangs(additionalLanguages);
+    this.translate.use(defaultLanguage);
   }
 
 
-  createModelData() {
-    let PageDetailsDataModel;
-    PageDetailsDataModel = new PageDetailsModel();
-    PageDetailsDataModel.language = this.pageData.language;
-    PageDetailsDataModel.header = this.pageData.header;
-    PageDetailsDataModel.details = this.pageData.details;
-    PageDetailsDataModel.pnr = this.pageData.pnr;
-    PageDetailsDataModel.pnrError = this.pageData.pnrError;
-    PageDetailsDataModel.pnrLabel = this.pageData.pnrLabel;
-    PageDetailsDataModel.firstname = this.pageData.firstname;
-    PageDetailsDataModel.lastname = this.pageData.lastname;
 
-    PageDetailsDataModel.firstnameError = this.pageData.firstnameError;
-    PageDetailsDataModel.lastnameError = this.pageData.lastnameError;
-    PageDetailsDataModel.firstnameLabel = this.pageData.firstnameLabel;
-    PageDetailsDataModel.lastnameLabel = this.pageData.lastnameLabel;
-    PageDetailsDataModel.submit = this.pageData.submit;
-    this.pageDetailsModels = PageDetailsDataModel;
-    // this.pageDetailsModels.push(PageDetailsDataModel);
-  }
   onSubmit(form: NgForm) {
     this.pageError = ""; this.pageSuccess = ""
     let self = this;
-    console.log(this.checkinDetailsModels);
-    console.log(JSON.stringify(this.checkinDetailsModels));
     this.checkinService.postCheckInDetails(JSON.stringify(this.checkinDetailsModels), this.lang)
       .subscribe(
       pageData => { console.log('Completed pageDataLoad!' + pageData); self.pageSuccess = "Success!!!" },
-      error => { console.log(error.text()); var data = JSON.parse(error.text()); self.pageError = data.Description; },
+      error => { console.log(error.text()); var data = JSON.parse(error.text()); self.pageError = data.Code+" : "+data.Description; },
       () => console.log('Completed pageDataLoad!')
       );
   }
 
-  // alert("submited");
-  //alert(JSON.stringify(this.checkinDetailsModels));
-
-  //arey now do css thing in your system and anyother fix i need to sedn
+ 
 
 }
